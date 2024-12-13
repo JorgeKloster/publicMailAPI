822eb504c13a35667ffc6ba581fd67f5e8978668
JorgeKloster <jfkloster@gmail.com>
2024-12-13 16:19:31 -0300

    edicao do arquivo vercel 4

--- server.js
+++ server.js
@@ -2,12 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const port = process.env.PORT || 10000
+const port = process.env.PORT || 3000
 
 const app = express();
 app.use(cors);
 app.use(bodyParser.json());
-app.options('*', cors());
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
 
 app.post('/api/contact', (req, res) => {
   const { name, email, message } = req.body;
@@ -37,6 +60,6 @@ app.post('/api/contact', (req, res) => {
   });
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
