619e9e057a8a3a0defc9d234cad7d2b57553d2dc
JorgeKloster <jfkloster@gmail.com>
2024-12-13 14:37:23 -0300

    mudanca no cors

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 10000
 const app = express();
 app.use(cors);
 app.use(bodyParser.json());
+app.options('*', cors());
 
 app.post('/api/contact', (req, res) => {
   const { name, email, message } = req.body;
